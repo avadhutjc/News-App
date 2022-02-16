f2232bc2851f14f8a85973a261455d4f9a526bc8
Avadhutjc <Avadhutjc@gmail.com>
2022-02-16 11:48:19 +0530

    designing UI better

--- app/src/main/java/com/ajc/lattice/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ajc/lattice/viewmodel/MainViewModel.kt
@@ -6,13 +6,16 @@ import androidx.paging.PagingData
 import com.ajc.lattice.model.remote.Article
 import com.ajc.lattice.repository.DataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val movieRepository: DataRepository) :
+class MainViewModel @Inject constructor(private val dataRepository: DataRepository) :
     ViewModel() {
 
     fun searchNews(): LiveData<PagingData<Article>> {
-        return movieRepository.getNewsResults()
+        return dataRepository.getNewsResults()
     }
+
 }
